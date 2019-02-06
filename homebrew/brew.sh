07078602aa062d37237841a2ecb0b0d2ae8fc5be
Daniel Ricardo <daniel@dynamicyield.com>
2019-02-06 10:07:48 +0200

    use Brewfile to contorl applications install

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -6,43 +6,19 @@ set -o pipefail
 if test ! "$( command -v brew )"; then
     echo "Installing homebrew"
     ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+    echo "Installing mas"
+    brew install mas
+fi
+
+if [ ! -f ~/.Brewfile ]; then
+  echo "Please create ~/.Brewfile first"
+  echo "You can use the link script to do it"
+  exit 1
 fi
 
 echo -e "\\n\\nInstalling homebrew packages..."
-echo "=============================="
-
-formulas=(
-    bat
-    git
-    'grep --with-default-names'
-    fish
-    hub
-    'macvim --with-override-system-vim'
-#    neovim
-    node
-    n
-    python
-    reattach-to-user-namespace
-    the_silver_searcher
-    shellcheck
-    tmux
-#    trash
-    wget
-    z
-    ripgrep
-#    git-standup
-    entr
-    git-secrets
-)
-
-for formula in "${formulas[@]}"; do
-    formula_name=$( echo "$formula" | awk '{print $1}' )
-    if brew list "$formula_name" > /dev/null 2>&1; then
-        echo "$formula_name already installed... skipping."
-    else
-        brew install "$formula"
-    fi
-done
+echo "==============================="
+brew bundle --global
 
 # make fish the default shell
 fish_path="$( command -v fish )"
