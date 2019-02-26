cc8e44099dc8e2789b52765a88c8c20cf6f8244c
Daniel Ricardo <daniel@dynamicyield.com>
2019-02-26 17:06:54 +0200

    fix type

--- fish/functions/mssh.fish
+++ fish/functions/mssh.fish
@@ -1,4 +1,4 @@
 # Defined in /Users/ricardo/.config/fish/functions/mssh.fish @ line 2
 function mssh
-  $DOTFILES/vin/mssh.sh -d (string join ' ' $argv)
+  $DOTFILES/bin/mssh.sh -d (string join ' ' $argv)
 end
