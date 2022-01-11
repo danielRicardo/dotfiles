a7aec835565846aac1192b0dd59a8ff8195d853b
daniel.ricardo <daniel.ricardo@appsflyer.com>
2022-01-11 15:58:08 +0200

    add git related plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,19 +1,26 @@
 return require("packer").startup(function()
 
+  use 'AndrewRadev/linediff.vim'
   use 'christoomey/vim-sort-motion'
   use 'christoomey/vim-tmux-navigator'
+  use 'junegunn/gv.vim'
   use 'mfussenegger/nvim-dap'
+  use 'mhinz/vim-signify'
   use 'neovim/nvim-lspconfig'
   use 'nvim-lua/plenary.nvim'
   use 'nvim-lua/popup.nvim'
   use 'nvim-telescope/telescope-fzy-native.nvim'
-  use 'nvim-telescope/telescope.nvim'
   use 'overcache/NeoSolarized'
-  use 'scalameta/nvim-metals'
+  use 'tpope/vim-fugitive'
   use 'tpope/vim-repeat'
+  use 'tpope/vim-rhubarb'
   use 'tpope/vim-surround'
-  use {'hrsh7th/nvim-compe', requires = {{'hrsh7th/vim-vsnip'}}}
-  use {'wbthomason/packer.nvim', opt = true}
+  use 'vim-test/vim-test'
+  use { 'hrsh7th/nvim-compe', requires = {{'hrsh7th/vim-vsnip'}} }
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}} }
+  use { 'scalameta/nvim-metals', requires = {{"nvim-lua/plenary.nvim"}} }
+
+  use { 'wbthomason/packer.nvim', opt = true}
 
 end)
   
