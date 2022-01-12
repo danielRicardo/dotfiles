59bd1976f9175d695f62cc84d72fcc1d7b982392
daniel.ricardo <daniel.ricardo@appsflyer.com>
2022-01-12 15:14:42 +0200

    categorize plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,27 +1,41 @@
 return require("packer").startup(function()
 
+  -- vim enhancements
   use 'AndrewRadev/linediff.vim'
   use 'christoomey/vim-sort-motion'
-  use 'christoomey/vim-tmux-navigator'
   use 'easymotion/vim-easymotion'
-  use 'junegunn/gv.vim'
-  use 'mfussenegger/nvim-dap'
-  use 'mhinz/vim-signify'
-  use 'neovim/nvim-lspconfig'
   use 'nvim-lua/plenary.nvim'
   use 'nvim-lua/popup.nvim'
   use 'nvim-telescope/telescope-fzy-native.nvim'
-  use 'overcache/NeoSolarized'
-  use 'tpope/vim-fugitive'
   use 'tpope/vim-repeat'
-  use 'tpope/vim-rhubarb'
   use 'tpope/vim-surround'
+  use 'vim-test/vim-test'
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}} }
+
+  -- git related
+  use 'junegunn/gv.vim'
+  use 'mhinz/vim-signify'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  
+  -- LSP
+  use 'mfussenegger/nvim-dap'
+  use 'neovim/nvim-lspconfig'
+  use { 'hrsh7th/nvim-compe', requires = {{'hrsh7th/vim-vsnip'}} }
+
+  -- scala
+  use { 'scalameta/nvim-metals', requires = {{"nvim-lua/plenary.nvim"}} }
+
+  -- clojure
+
+  -- look and feel
+  use 'overcache/NeoSolarized'
   use 'vim-airline/vim-airline'
   use 'vim-airline/vim-airline-themes'
-  use 'vim-test/vim-test'
-  use { 'hrsh7th/nvim-compe', requires = {{'hrsh7th/vim-vsnip'}} }
-  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}} }
-  use { 'scalameta/nvim-metals', requires = {{"nvim-lua/plenary.nvim"}} }
+
+  -- misc
+  use 'christoomey/vim-tmux-navigator'
+
 
   use { 'wbthomason/packer.nvim', opt = true}
 
