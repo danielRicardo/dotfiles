48efa737468b9d4a99e4600c87a0498c698a635e
Daniel Ricardo <daniel.k.ricardo@gmail.com>
2025-03-09 21:56:49 +0200

    add a few more plugins to support clojure

---
+++ nvim/.config/nvim/lua/plugins/clojure.lua
@@ -0,0 +1,13 @@
+local vimJackIn = {
+  "clojure-vim/vim-jack-in",
+  dependencies = {
+    "tpope/vim-dispatch",
+    "radenling/vim-dispatch-neovim",
+  },
+}
+
+local sexp_mappings = {
+  "tpope/vim-sexp-mappings-for-regular-people",
+}
+
+return { vimJackIn, sexp_mappings }
